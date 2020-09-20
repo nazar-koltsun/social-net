65225febfc51f7c27d908cda7fc22752a1fd9d80
nazar_k <nazar.koltsun@plumrocket.com>
2020-09-20 21:38:45 +0300

    lesson 74

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -19,6 +19,16 @@ class ProfileStatus extends React.Component {
         this.setState({status: e.currentTarget.value});
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            });
+        }
+
+        console.log("componentDidUpdate");
+    }
+
     render() {
         console.log(this.props);
         return (
