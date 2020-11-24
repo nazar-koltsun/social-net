444a51123ec9f2fd916808e2aa45f3d6e5576076
nazar_k <nazar.koltsun@plumrocket.com>
2020-11-24 09:06:57 +0200

    Lesson 85

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 const ProfileStatusWithHooks = (props) => {
     console.log(props);
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
+
+    useEffect( () => {
+        setStatus(props.status);
+    }, [props.status] );
 
     const activateMode = () => {
         setEditMode(true);
